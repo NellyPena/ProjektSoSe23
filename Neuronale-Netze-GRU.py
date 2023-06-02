e35af870bc05b153f9e8e95df90d79dcf6bc4ee1
Nelly Pena <nelly.pguajardo@student.uni-siegen.de>
2023-06-02 13:47:21 +0200

    Does not run anymore after adding comments!

--- Neuronale-Netze-GRU.py
+++ Neuronale-Netze-GRU.py
@@ -4,11 +4,12 @@ import pandas as pd
 
 
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Dropout,GRU
+from tensorflow import keras 
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,GRU
 
 #Cargar los datos
-company = 'TSLA'
+company = 'TSLA' #used in the graph plot
 #hist = pd.read_csv('TSLA.csv') #2012 - 2019
 df = pd.read_csv('TSLA.csv')
 hist =np.array(df[:int(df.shape[0]*0.7)])
@@ -21,59 +22,77 @@ scaled_data = scaler.fit_transform(hist['Close'].values.reshape(-1,1))
 
 prediction_days = 60
 
-x_train = []
-y_train = []
+x_train = [] #prepared training data
+y_train = [] #prepared training data
 
-for x in range(prediction_days, len(scaled_data)):
-    x_train.append(scaled_data[x-prediction_days:x, 0])
-    y_train.append(scaled_data[x,0])
+for x in range(prediction_days, len(scaled_data)): #from prediction days value until the end of the train test array
+    x_train.append(scaled_data[x-prediction_days:x, 0]) #we take the first 60 values
+    y_train.append(scaled_data[x,0]) #the 61st value
 
-x_train, y_train = np.array(x_train),np.array(y_train)
-x_train = np.reshape(x_train,(x_train.shape[0], x_train.shape[1],1))
+x_train, y_train = np.array(x_train),np.array(y_train) # converted into numpy arrays
+x_train = np.reshape(x_train,(x_train.shape[0], x_train.shape[1],1)) #reshape so it can work with the neural network
 
 print(x_train.shape)
 
 #construir el modelo
 model = Sequential()
 
-model.add(GRU(units=50, return_sequences = True, input_shape=(x_train.shape[1],1)))
+#layer specification
+model.add(GRU(units=50, return_sequences = True, input_shape=(x_train.shape[1],1))) #units can be changed, return = true for recurrent neural networks (as LSTM feeds information back, not only forward)
+#input shape is...
 model.add(Dropout(0.2))
 model.add(GRU(units=50, return_sequences = True))
 model.add(Dropout(0.2))
 model.add(GRU(units=50))
 model.add(Dropout(0.1))
-model.add(Dense(units=1))
+model.add(Dense(units=1)) #prediction of the next closing price
 
-model.compile(optimizer='adam', loss='mean_squared_error')
+#many layers lead to overfitting
 
-model.fit(x_train,y_train, epochs=25, batch_size=32)
+model.compile(optimizer='adam', loss='mean_squared_error') #loss function : mean squared error
 
+model.fit(x_train,y_train, epochs=25, batch_size=32) #batch size: the model is going to see 32 units at once all the time, 
+
+#######################################test model accuracy on existing data
 #cargar los datos de test
 #hist_test = pd.read_csv('TSLA_testdata.csv') #2020 to date
-hist_test = np.array(df[int(df.shape[0]*0.7):])
+hist_test = np.array(df[int(df.shape[0]*0.7):]) #new data, not used before
 
 actual_prices  = hist_test['Close'].values
 
-total_dataset = pd.concat((hist['Close'],hist_test['Close']),axis=0)
-model_inputs = total_dataset[len(total_dataset)-len(hist_test)-prediction_days:].values
-model_inputs = scaler.transform(model_inputs.reshape(-1,1))
+#we need to get and scale the prices
+total_dataset = pd.concat((hist['Close'],hist_test['Close']),axis=0)  #data set that combines the closing data (not-scaled) with closed values test data
+model_inputs = total_dataset[len(total_dataset)-len(hist_test)-prediction_days:].values #we want to start as soon as possible
+model_inputs = scaler.transform(model_inputs.reshape(-1,1))  #to scale down
 
-####
+#### prediction based on the test data
 x_test = []
-for x in range(prediction_days,len(model_inputs)):
+
+for x in range(prediction_days,len(model_inputs)): # len(model_inputs)+1 to get the newest one?
     x_test.append(model_inputs[x-prediction_days:x,0])
 
-x_test = np.array(x_test)
-x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
+x_test = np.array(x_test) #transform to array
+x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1)) #same format as x_train
 
-predicted_prices = model.predict(x_test)
-predicted_prices = scaler.inverse_transform(predicted_prices)
+#### actual prediction
+predicted_prices = model.predict(x_test) #remember: the prediction prices are scaled
+predicted_prices = scaler.inverse_transform(predicted_prices) #un-scale the predicted prices
 
 ##plot
-plt.plot(actual_prices,color="black",label=f"{company} real prices")
-plt.plot(predicted_prices,color="blue",label=f"{company} predicted prices")
+plt.plot(actual_prices, color="black", label=f"{company} real prices")
+plt.plot(predicted_prices, color="blue", label=f"{company} predicted prices")
+plt.title(f"{company} Share Price Vs Prediction") #plot not showing after adding this line
 plt.legend()
 plt.show()
+
+#PREDICT NEXT DAY
+real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs + 1),0]]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data,(real_data.shape[0],real_data.shape[1],1))
+
+prediction =model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(f"Prediction (for the next day): {prediction}")
 
 ##rentability, only when going LONG
 rentability = 1
