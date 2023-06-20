d03901bcf63a6850bf03e79f893707e24b074ccb
Nelly Pena <nelly.pguajardo@student.uni-siegen.de>
2023-06-21 01:39:12 +0200

    GRU + Vorhersagekurve + SME + .csv der Vorhersage

--- Neuronale-Netze-GRU.py
+++ Neuronale-Netze-GRU.py
@@ -8,8 +8,9 @@ from keras.models import Sequential, load_model
 from keras.layers import GRU, Dense, Dropout
 from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv('DAX.csv')
-company = 'DAX'
+df = pd.read_csv('ALV.csv')
+company = 'ALV'
+window_size = 50
 
 df = df['Close'].values #Open
 df = df.reshape(-1, 1)
@@ -25,8 +26,8 @@ dataset_test = scaler.transform(dataset_test)
 def create_dataset(df):
     x = []
     y = []
-    for i in range(50, df.shape[0]): #50 affects len(prediction) , len(prediction) = len(x_test) + len(window)
-        x.append(df[i-50:i, 0])  #50
+    for i in range(window_size, df.shape[0]): #50 affects len(prediction) , len(prediction) = len(x_test) + len(window)
+        x.append(df[i-window_size:i, 0])  #50
         y.append(df[i, 0])
     x = np.array(x)
     y = np.array(y)
@@ -42,27 +43,28 @@ model = Sequential()
 #layer specification
 model.add(GRU(units=50, return_sequences = True, input_shape=(x_train.shape[1],1))) #units can be changed, return = true for recurrent neural networks (as LSTM feeds information back, not only forward)
 #input shape is...
-model.add(Dropout(0.2))
+model.add(Dropout(0.2)) #0.2
 model.add(GRU(units=50, return_sequences = True))
-model.add(Dropout(0.2))
+model.add(Dropout(0.2)) #0.2
 model.add(GRU(units=50))
-model.add(Dropout(0.1))
+model.add(Dropout(0.2)) #0.1
 model.add(Dense(units=1)) #prediction of the next closing price
 
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(x_train, y_train, epochs=5, batch_size=32) #epochs25
-#model.save('stock_prediction.h5')#model training operations, not required.
+model.fit(x_train, y_train, epochs=25, batch_size=32) #epochs25
+model.save('stock_prediction.h5') #model training operations, not required.
 
-#model = load_model('stock_prediction.h5')
+model = load_model('stock_prediction.h5')
 
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions)
 y_test_scaled = scaler.inverse_transform(y_test.reshape(-1, 1))
 
-X_FUTURE = 50 #always using x[-1] from x_test for the prediction
+
+X_FUTURE = 10 #always using x[-1] from x_test for the prediction
 future_predictions = np.array([])
 last = x_test[-1]
 for i in range(X_FUTURE):
@@ -73,15 +75,24 @@ for i in range(X_FUTURE):
 future_predictions = scaler.inverse_transform([future_predictions])[0]
 print(future_predictions)
 
+###join lines 
+dicts = []
+last_day = len(y_test)-1 #-1 -2 reduces gap but means prediction on same day from both lines
+for i in range(X_FUTURE):
+  last_day = last_day + 1
+  dicts.append({'Predictions':future_predictions[i], "Date": last_day})
+
+new_data = pd.DataFrame(dicts).set_index("Date")
 
 #### Mean Squared Error
 mse = mean_squared_error(y_test_scaled, predictions)
 print("Mean Squared Error:", mse)
 
 #### PLOT
-plt.plot(future_predictions, color="tomato", label=f"{company} future X days prices")
-plt.plot(y_test_scaled, color="black", label=f"{company} real prices")
-plt.plot(predictions, color="steelblue", label=f"{company} predicted prices")
+#plt.plot(future_predictions, color="tomato", label=f"{company} future X days prices")
+plt.plot(y_test_scaled, color="black", label=f"{company} Real Prices")
+plt.plot(predictions, color="steelblue", label=f"{company} Predicted Prices")
+plt.plot(new_data, color="tomato", label=f"{company} Future {X_FUTURE} Days")
 plt.title(f"{company} Share Price Vs Prediction") #plot not showing after adding this line
 plt.legend()
 plt.show()
